73248274c485838510f9822fc8f6ca89325b05e5
Arjun Jain <113439211+arjun28jain@users.noreply.github.com>
2024-07-29 03:29:33 +0530

    Update Card.jsx

--- src/componets/Card.jsx
+++ src/componets/Card.jsx
@@ -5,8 +5,8 @@ import { motion } from "framer-motion"
 function Card({reference}) {
   return (
     <motion.div drag dragConstraints={reference} className='h-[18rem] w-[14rem] bg-zinc-900/50 left-[20px] right-[20px] m-[10px] rounded-xl'>
-      <div className=' h-[2rem] w-[12rem]  text-center text-[1.6vw] text-orange-500 m-[10px] font-semibold' > TITLE</div>
-         <div className='text-zinc-200 p-2 m-[2px] '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque, sunt incidunt! Delectus, placeat suscipit! Magni adipisci ab sunt, nesciunt esse voluptatem officiis</div> 
+      <div className=' h-[2rem] w-[12rem]  text-center text-[1.6vw] text-orange-500 m-[10px] font-semibold' > Drag Me!!</div>
+         <div className='text-zinc-200 p-2 m-[2px] '>Hey!! Welcome to my docsapp web application. Here you can the funny floating note heads just by dragging them wherever you want!!</div> 
           
     
     
